c65bc8146bf4b95d277e4f7ea72bee498c0c56d8
Belen <elbe.enriquez@gmail.com>
2022-09-09 20:11:20 -0300

    primer commit

---
+++ deliverar-repartidor/src/main/java/com/uade/deliverarrepartidor/DeliverarRepartidorApplication.java
@@ -0,0 +1,13 @@
+package com.uade.deliverarrepartidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverarRepartidorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliverarRepartidorApplication.class, args);
+    }
+
+}
---
+++ deliverar-repartidor/src/test/java/com/uade/deliverarrepartidor/DeliverarRepartidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.deliverarrepartidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliverarRepartidorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
